e90dabd8bd8f048862412d971c8c90d38bc3028b
Elian Joel Ruiz Callejas <ejruizca@crystal.com.co>
2025-10-21 07:55:25 -0500

    commit

---
+++ global.d.ts
@@ -0,0 +1,25 @@
+// Allow importing CSS modules (e.g. *.module.css)
+declare module "*.css" {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module "*.module.css" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module "*.module.scss" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module "*.module.sass" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.svg";
